562f01f1f8946db432009ce347aba91ebfc6dda5
Rowan-Paul <rpflynn80@gmail.com>
2021-05-03 11:13:37 +0200

    Fix navBar hanging

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 function NavBarUI(props) {
   return (
-    <div className="fixed top-0 right-0 my-2 mx-5">
+    <div className="absolute top-0 right-0 my-2 mx-5">
       {props.signedIn ? (
         <Link to="/signout">Sign out</Link>
       ) : (
