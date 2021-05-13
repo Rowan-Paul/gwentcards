bbdd6730ea9465e20ded7b240c1701c1f6cf34d1
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-13 09:58:25 +0200

    Add service worker

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { persistor, store } from './redux/store'
 import { App } from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 require('dotenv').config()
 
@@ -30,3 +31,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
