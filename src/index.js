f9b030cdfc752216e423e0f70578a5ff3b55fe1e
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-21 17:35:36 +0200

    Fix store not being imported in index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import { App } from './App'
+import store from './redux/store'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
