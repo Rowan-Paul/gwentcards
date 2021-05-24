6419c967e360745bfca679a3c9c75da9af6e1cf0
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-24 19:48:03 +0200

    Fix close button & overflow for modal

--- src/components/cards/LocationsModal.js
+++ src/components/cards/LocationsModal.js
@@ -45,7 +45,7 @@ function LocationsModalUI(props) {
     <div
       className={`${
         props.showLocationModal ? 'fixed' : 'hidden'
-      } inset-0 mx-5 lg:mx-72 my-10 p-5 rounded bg-white shadow border-2 z-40`}
+      } inset-0 mx-5 lg:mx-72 my-10 p-5 rounded overflow-auto bg-white shadow border-2 z-40`}
     >
       <span
         className="block text-center ml-5 float-right cursor-pointer sticky top-0 right-0"
@@ -70,13 +70,13 @@ function LocationsModalUI(props) {
           <tbody>{locations}</tbody>
         </table>
         <span className="block md:hidden">{locationsMobile}</span>
+        <span
+          className="text-white px-4 py-2 block float-right bg-red-600  rounded hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none cursor-pointer"
+          onClick={() => props.setLocationModal()}
+        >
+          Close
+        </span>
       </div>
-      <span
-        className="text-white px-4 py-2 block float-right bg-red-600  rounded hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none cursor-pointer"
-        onClick={() => props.setLocationModal()}
-      >
-        Close
-      </span>
     </div>
   )
 }
