c26fcbcf29c4db0ca469ffa033826e9786be3d59
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-25 16:15:14 +0200

    Add contact email to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,27 +15,31 @@ function FooterUI(props) {
     return ''
   } else {
     return (
-      <footer className="grid md:grid-cols-5">
-        <span className="col-span-5">
+      <footer className="grid grid-cols-1 md:grid-cols-6 text-left md:text-center">
+        <span className="md:col-span-6">
           Copyright &copy; {new Date().getFullYear()} Rowan Paul Flynn <br></br>
         </span>
-        <span className="col-span-5 md:col-span-1 md:col-start-2">
+        <span className="md:col-start-2">
           <a href="https://github.com/Rowan-Paul/gwentcards">
             Frontend repository
           </a>
         </span>
-        <span className="col-span-5 md:col-span-1 ">
+        <span>
           <a href="https://github.com/Rowan-Paul/gwentcards">
             Backend repository
           </a>
         </span>
 
-        <span className="col-span-5 md:col-span-1 md:col-end-5">
+        <span>
           {props.signedIn ? (
             <Link to="/remove-account">Remove account</Link>
           ) : (
             <Link to="/signup">Create account</Link>
           )}
+        </span>
+
+        <span className="md:col-end-6">
+          <a href="mailto:contact@rowanpaulflynn.com">Contact us</a>
         </span>
       </footer>
     )
