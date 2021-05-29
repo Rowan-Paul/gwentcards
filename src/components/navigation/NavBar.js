0d85a5c56c02fb222f68bdf1a69750f509420901
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-29 13:05:48 +0200

    Add home to nav

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,16 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 function NavBarUI(props) {
-  return (
-    <div className="absolute top-0 right-0 my-2 mx-5">
-      {props.signedIn ? (
-        <Link to="/signout">Sign out</Link>
-      ) : (
-        <Link to="/signin">Sign In</Link>
-      )}
-    </div>
-  )
+  const noFooterPages = [
+    '/signin',
+    '/signup',
+    '/remove-account',
+    '/reset-password',
+    '/verify-account',
+  ]
+  const location = useLocation()
+
+  if (noFooterPages.includes(location.pathname)) {
+    return (
+      <div className="absolute py-2 px-5 w-full">
+        <Link to="/">Home</Link>
+        {props.signedIn ? (
+          <Link to="/signout" className="float-right">
+            Sign out
+          </Link>
+        ) : (
+          <Link to="/signin" className="float-right">
+            Sign In
+          </Link>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div className="absolute top-0 right-0 my-2 mx-5">
+        {props.signedIn ? (
+          <Link to="/signout">Sign out</Link>
+        ) : (
+          <Link to="/signin">Sign In</Link>
+        )}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
