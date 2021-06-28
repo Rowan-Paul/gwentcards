9f4acc835470149a30b2141f4832e79055e321ba
Rowan-Paul <contact@rowanpaulflynn.com>
2021-06-28 10:26:19 +0200

    Remove heart icon

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -22,19 +22,6 @@ function CardUI(props) {
       <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
     </svg>
   )
-  const heartIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="24px"
-      viewBox="0 0 24 24"
-      width="24px"
-      fill="#000000"
-      className="h-6 w-6 float-right mr-3 mb-5 cursor-pointer"
-    >
-      <path d="M0 0h24v24H0V0z" fill="none" />
-      <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
-    </svg>
-  )
 
   const { card } = props
   const abilities = []
@@ -109,7 +96,6 @@ function CardUI(props) {
             Notes
           </span>
 
-          {heartIcon}
           <span
             className="h-6 w-6 float-right mr-5 mb-5 relative text-sm hover:underline cursor-pointer"
             onClick={() => setShowLocationModal(!showLocationModal)}
