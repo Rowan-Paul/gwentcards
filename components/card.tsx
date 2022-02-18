4d4e2d05c71567a256d73dbb074eaf7f9e547c55
Rowan-Paul <contact@rowanpaulflynn.com>
2022-02-18 16:33:55 +0100

    Add collect button

--- components/card.tsx
+++ components/card.tsx
@@ -25,36 +25,45 @@ const Card = ({ image, name, deck, strength, row }: ICardProps): JSX.Element => 
 
 const DesktopCard = ({ image, name, deck, strength, row }: ICardProps) => {
   return (
-    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 hidden md:grid grid-cols-7 justify-center items-center gap-8 p-3">
-      <div>
-        <Image src={image} alt={`${name} card`} width="75" height="142" />
+    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 hidden md:block p-3">
+      <div className="md:grid grid-cols-6 justify-center items-center gap-8">
+        <div>
+          <Image src={image} alt={`${name} card`} width="75" height="142" />
+        </div>
+        <Column type="Card" value={name} span={2} />
+        <Column type="Deck" value={deck} />
+        <Column type="Strength" value={strength} />
+        <Column type="Row" value={row} />
       </div>
-      <Column type="Card" value={name} span={2} />
-      <Column type="Deck" value={deck} />
-      <Column type="Strength" value={strength} />
-      <Column type="Row" value={row} />
-      <div className="text-center">
-        <input type="checkbox" />
-      </div>
+      <CollectButton />
     </div>
   );
 };
 
 const MobileCard = ({ image, name, deck, strength, row }: ICardProps) => {
   return (
-    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 md:hidden grid grid-cols-4 justify-center items-center gap-4 p-3">
-      <div>
-        <Image src={image} alt={`${name} card`} width="75" height="142" />
+    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 md:hidden p-3">
+      <div className="grid grid-cols-4 justify-center items-center gap-4">
+        <div>
+          <Image src={image} alt={`${name} card`} width="75" height="142" />
+        </div>
+        <div className="flex flex-col gap-2 col-span-2">
+          <Column type="Card" value={name} />
+          <Column type="Deck" value={deck} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Column type="Strength" value={strength} />
+          <Column type="Row" value={row} />
+        </div>
       </div>
-      <div className="flex flex-col gap-2 col-span-2">
-        <Column type="Card" value={name} />
-        <Column type="Deck" value={deck} />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Column type="Strength" value={strength} />
-        <Column type="Row" value={row} />
-      </div>
+      <CollectButton />
     </div>
+  );
+};
+
+const CollectButton = () => {
+  return (
+    <button className="text-center w-full bg-indigo-400 drop-shadow-lg p-2 my-2 hover:bg-indigo-300">Collect</button>
   );
 };
 
