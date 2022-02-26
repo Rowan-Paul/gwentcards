2e3ed9c0d05d75c967f0c262a70d0482cf274699
Rowan-Paul <contact@rowanpaulflynn.com>
2022-02-26 21:55:02 +0100

    Add click to enlarge image

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import CollectButton from './CollectButton';
 
 interface ICardProps {
@@ -20,33 +21,82 @@ interface IColumnProps {
 
 const Card = ({ card }: ICardProps) => {
   const { id, image, name, deck, strength, row } = card;
+  const [showImage, setShowImage] = useState(false);
+  const wrapperRef: any = useRef(null);
+
+  const escFunction = useCallback(
+    (event) => {
+      if (event.key === 'Escape' && showImage) {
+        setShowImage(false);
+      }
+    },
+    [showImage]
+  );
+
+  const handleClickOutside = useCallback(
+    (event) => {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target) && showImage) {
+        setShowImage(false);
+      }
+    },
+    [showImage]
+  );
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', escFunction, false);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', escFunction, false);
+    };
+  }, [wrapperRef, handleClickOutside, escFunction]);
+
+  useEffect(() => {
+    if (showImage) {
+      document.body.style.overflow = 'hidden';
+    }
+  }, [showImage]);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
 
   return (
-    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 p-3 drop-shadow-md">
-      <div className="hidden md:grid grid-cols-6 justify-center items-center gap-8">
-        <div>
-          <Image src={image} alt={`${name} card`} width="75" height="142" />
-        </div>
-        <Column type="Card" value={name} span={2} />
-        <Column type="Deck" value={deck} />
-        {strength && <Column type="Strength" value={strength} />}
-        <Column type="Row" value={row.charAt(0).toUpperCase() + row.slice(1)} />
-      </div>
-      <div className="grid md:hidden grid-cols-4 justify-center items-center gap-4">
-        <div>
-          <Image src={image} alt={`${name} card`} width="75" height="142" />
-        </div>
-        <div className="flex flex-col gap-2 col-span-2">
-          <Column type="Card" value={name} />
+    <>
+      <div className="bg-gradient-to-r from-cyan-400 to-blue-400 p-3 drop-shadow-md">
+        <div className="hidden md:grid grid-cols-6 justify-center items-center gap-8">
+          <div onClick={() => setShowImage(true)}>
+            <Image src={image} alt={`${name} card`} width="75" height="142" />
+          </div>
+          <Column type="Card" value={name} span={2} />
           <Column type="Deck" value={deck} />
-        </div>
-        <div className="flex flex-col gap-2">
           {strength && <Column type="Strength" value={strength} />}
           <Column type="Row" value={row.charAt(0).toUpperCase() + row.slice(1)} />
         </div>
+        <div className="grid md:hidden grid-cols-4 justify-center items-center gap-4">
+          <div onClick={() => setShowImage(true)}>
+            <Image src={image} alt={`${name} card`} width="75" height="142" />
+          </div>
+          <div className="flex flex-col gap-2 col-span-2">
+            <Column type="Card" value={name} />
+            <Column type="Deck" value={deck} />
+          </div>
+          <div className="flex flex-col gap-2">
+            {strength && <Column type="Strength" value={strength} />}
+            <Column type="Row" value={row.charAt(0).toUpperCase() + row.slice(1)} />
+          </div>
+        </div>
+        <CollectButton id={id} />
       </div>
-      <CollectButton id={id} />
-    </div>
+      <div
+        className={showImage ? 'absolute backdrop-blur w-screen h-screen z-40' : 'hidden'}
+        onKeyPress={() => setShowImage(false)}
+      >
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50" ref={wrapperRef}>
+          <Image src={image} alt={`${name} card`} width="250" height="500" />
+        </div>
+      </div>
+    </>
   );
 };
 
