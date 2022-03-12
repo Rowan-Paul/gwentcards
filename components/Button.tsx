ad7397f92f5061e94098f8415e2e9016ca537b46
Rowan-Paul <contact@rowanpaulflynn.com>
2022-03-12 21:47:02 +0100

    Update selected button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,8 +8,8 @@ const Button = ({ onClick, title, selected }: IButtonProps) => {
   return (
     <div
       onClick={onClick}
-      className={`block text-center text-white p-4 bg-purple-500 drop-shadow-lg my-2 cursor-pointer divide-purple-400 divide-x divide-solid hover:bg-indigo-600 select-none ${
-        selected && 'border-4 border-purple-800'
+      className={`text-center text-white p-4 bg-purple-500 drop-shadow-lg my-2 cursor-pointer hover:bg-indigo-600 select-none ${
+        selected && 'bg-purple-800'
       }`}
     >
       {title}
