c2ac701a97d7d539221f0010a874321b503b9ac1
Rowan-Paul <contact@rowanpaulflynn.com>
2022-03-28 19:34:25 +0200

    Fix button contrast

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ onClick, title, selected }: IButtonProps) => {
   return (
     <div
       onClick={onClick}
-      className={`text-center text-white p-4 bg-purple-500 drop-shadow-lg my-2 cursor-pointer hover:bg-indigo-600 select-none ${
+      className={`text-center text-white p-4 bg-purple-600 drop-shadow-lg my-2 cursor-pointer hover:bg-indigo-700 select-none ${
         selected && 'bg-purple-800'
       }`}
       tabIndex={0}
