70ecc48183338bb5dcab4754ba2f2db3169d7cba
Rowan-Paul <contact@rowanpaulflynn.com>
2023-01-31 13:56:49 +0100

    Remove header to old website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,6 @@ import '/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const [hideBanner, setHideBanner] = useState<boolean>(true);
-
-  useEffect(() => {
-    setHideBanner(localStorage.hideBanner);
-  }, [hideBanner]);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -35,24 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <meta property="og:image" content="/icon.png" />
           <meta property="og:url" content="https://gwentcards.net" />
         </Head>
-
-        {!hideBanner && (
-          <div className="bg-gradient-to-r from-cyan-700 to-indigo-400 p-3 flex justify-end text-white">
-            <a href="https://old.gwentcards.net/" className="mr-auto justify-center">
-              Looking for the old site with accounts and offline functionality? Go to&nbsp;
-              <span className="underline">old.gwentcards.net</span>.
-            </a>
-            <span
-              className="text-sm cursor-pointer"
-              onClick={() => {
-                localStorage.setItem('hideBanner', 'true');
-                setHideBanner(true);
-              }}
-            >
-              (close)
-            </span>
-          </div>
-        )}
+        
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
       </Hydrate>
